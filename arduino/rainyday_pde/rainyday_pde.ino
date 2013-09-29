3d5906c4b5b47df2b6626ddfa7f2c77b86d5da1d
Jules Dourlens <jdourlens@gmail.com>
2013-09-29 17:11:48 +0200

    Added pde file

---
+++ arduino/rainyday_pde/rainyday_pde.ino
@@ -0,0 +1,49 @@
+/*
+ * Firmata is a generic protocol for communicating with microcontrollers
+ * from software on a host computer. It is intended to work with
+ * any host computer software package.
+ *
+ * To download a host software package, please clink on the following link
+ * to open the download page in your default browser.
+ *
+ * http://firmata.org/wiki/Download
+ */
+
+/* This firmware supports as many servos as possible using the Servo library 
+ * included in Arduino 0017
+ *
+ * TODO add message to configure minPulse/maxPulse/degrees
+ *
+ * This example code is in the public domain.
+ */
+ 
+#include <Servo.h>
+#include <Firmata.h>
+
+Servo servos;
+
+void analogWriteCallback(byte pin, int value)
+{
+    if (pin == 10) {
+        servos[PIN_TO_SERVO(pin)].write(value);
+    }
+}
+
+void setup() 
+{
+    byte pin;
+
+    Firmata.setFirmwareVersion(0, 2);
+    Firmata.attach(ANALOG_MESSAGE, analogWriteCallback);
+
+    servos.attach(PIN_TO_DIGITAL(10));
+   
+    Firmata.begin(57600);
+}
+
+void loop() 
+{
+    while(Firmata.available())
+        Firmata.processInput();
+}
+
