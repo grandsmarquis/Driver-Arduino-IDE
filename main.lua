19da515e52ee3b1baaaeaea4fe4005b39528b964
Jules Dourlens <jdourlens@gmail.com>
2013-09-29 18:05:30 +0200

    Firmata working

--- main.lua
+++ main.lua
@@ -12,7 +12,22 @@ end
 function start()
    loadLibs()
    arduino = Arduino("/dev/ttyACM0")
-   arduino.analogWrite(10, 40)
+
+   arduino.pinMode(ledPin, ArduinoConstants.OUTPUT)
+
+   arduino.pinMode(3, ArduinoConstants.INPUT);
+   arduino.digitalWrite(3, ArduinoConstants.HIGH); --turns on pull-up resistor after input
+
+   while arduino ~= nil do
+      print(arduino.digitalRead(3))
+  --    arduino.digitalWrite(ledPin, not arduino.digitalRead(buttonPin))
+      --arduino.digitalWrite(13, ArduinoConstants.LOW)
+   end
 end
 
+--Global defs
+local wasActivated = false
+ledPin = 13
+
+--Entry point
 start()
