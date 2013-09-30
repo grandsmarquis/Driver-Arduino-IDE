bb7e6623dd6ee23c71c8e2401f654fa2cc1a5b0f
Jules Dourlens <jdourlens@gmail.com>
2013-10-01 01:53:06 +0200

    Background

--- main.lua
+++ main.lua
@@ -9,8 +9,15 @@ DEFAULT = {
 device = DEFAULT.device
 connectionStatus = "not"
 
-function loadLibs()
+ELEMS = {}
 
+
+
+function loadElements()
+   require "toolbar"
+end
+
+function loadLibs()
    local simple_dispatcher = require "lua-firmata.simple_dispatcher"
    local arduino_module = require "lua-firmata.arduino"
    local socket = require "socket"
@@ -32,79 +39,13 @@ function loadArduino(path)
    updateConnetionStatus()
 end
 
-function updateConnetionStatus()
-   if connectionStatus == "good" then
-      infoConnection:SetText({{0, 255, 0, 255}, "Connection status : \"Connected\""})
-      devInfoButton:SetClickable(true)
-   else
-      infoConnection:SetText({{255, 0, 0, 255}, "Connection status : \"Not connected\""})
-      devInfoButton:SetClickable(false)
-   end
-end
-
-function makeTaskBar()
-   local width = love.graphics.getWidth()
-   
-   local toolbar = loveframes.Create("panel")
-   toolbar:SetSize(width, 35)
-   toolbar:SetPos(0, 0)
-
-   info = loveframes.Create("text", toolbar)
-   info:SetPos(5, 3)
-   info:SetText({{0, 0, 0, 255}, "Device selected : \"" .. DEFAULT.device .. "\""})
-
-   infoConnection = loveframes.Create("text", toolbar)
-   infoConnection:SetPos(5, 18)
-   infoConnection:SetText({{0, 0, 0, 255}, "Connection status : \"\""})
-
-   local devButton = loveframes.Create("button", toolbar)
-   devButton:SetPos(toolbar:GetWidth() - 215, 5)
-   devButton:SetSize(100, 25)
-   devButton:SetText("Choose device")
-   devButton.OnClick = function()
-			  makeChooseDevice()
-		       end
-
-   devInfoButton = loveframes.Create("button", toolbar)
-   devInfoButton:SetPos(toolbar:GetWidth() - 105, 5)
-   devInfoButton:SetSize(100, 25)
-   devInfoButton:SetText("Device infos")
-   devInfoButton.OnClick = function()
-			      makeDeviceInfo()
-			   end
-end
-
-function makeDeviceInfo()
-
-end
-
-function makeChooseDevice()
-      local frame = loveframes.Create("frame")
-      frame:SetName("Path to device")
-      frame:SetSize(500, 90)
-      frame:SetModal(true)
-      local textinput = loveframes.Create("textinput", frame)
-      textinput:SetPos(5, 30)
-      textinput:SetWidth(490)
-      textinput.OnEnter = function(object)
-			     loadArduino(textinput:GetText())
-			     frame:Remove()
-			  end
-      textinput:SetFont(love.graphics.newFont(12))      
-      local togglebutton = loveframes.Create("button", frame)
-      togglebutton:SetPos(5, 60)
-      togglebutton:SetWidth(490)
-      togglebutton:SetText("OK")
-      togglebutton.OnClick = function(object)
-				loadArduino(textinput:GetText())
-				frame:Remove()
-			     end
-end
-
 function love.load()
+   love.graphics.setBackgroundColor( 50, 40, 150)
    loadLibs()
-   makeTaskBar()
+   loadElements()
+   makeToolbar()
    loadArduino(DEFAULT.device)
+   imageBG = love.graphics.newImage("images/bg.jpg")
 end
 
 function love.update(dt)
@@ -112,6 +53,15 @@ function love.update(dt)
 end
                  
 function love.draw()
+
+   local width = love.graphics.getWidth()
+   local height = love.graphics.getHeight()
+   local scalex = width/imageBG:getWidth()
+   local scaley = height/imageBG:getHeight()
+   
+   love.graphics.setColor(255, 255, 255, 255)
+   love.graphics.draw(imageBG, 0, 0, 0, scalex, scaley)
+
    loveframes.draw()
 end
  
---
+++ toolbar.lua
@@ -0,0 +1,80 @@
+function updateConnetionStatus()
+   if connectionStatus == "good" then
+      infoConnection:SetText({{0, 255, 0, 255}, "Connection status : \"Connected\""})
+      devInfoButton:SetClickable(true)
+      objectsButton:SetClickable(true)
+   else
+      infoConnection:SetText({{255, 0, 0, 255}, "Connection status : \"Not connected\""})
+      devInfoButton:SetClickable(false)
+      objectsButton:SetClickable(false)
+   end
+end
+
+
+function makeToolbar()
+   local width = love.graphics.getWidth()
+
+   local toolbar = loveframes.Create("panel")
+   toolbar:SetSize(width, 35)
+   toolbar:SetPos(0, 0)
+
+   info = loveframes.Create("text", toolbar)
+   info:SetPos(5, 3)
+   info:SetText({{0, 0, 0, 255}, "Device selected : \"" .. DEFAULT.device .. "\""})
+
+   infoConnection = loveframes.Create("text", toolbar)
+   infoConnection:SetPos(5, 18)
+   infoConnection:SetText({{0, 0, 0, 255}, "Connection status : \"\""})
+
+   local devButton = loveframes.Create("button", toolbar)
+   devButton:SetPos(toolbar:GetWidth() - 315, 5)
+   devButton:SetSize(100, 25)
+   devButton:SetText("Choose device")
+   devButton.OnClick = function()
+                          makeChooseDevice()
+                       end
+
+   devInfoButton = loveframes.Create("button", toolbar)
+   devInfoButton:SetPos(toolbar:GetWidth() - 210, 5)
+   devInfoButton:SetSize(100, 25)
+   devInfoButton:SetText("Device infos")
+   devInfoButton.OnClick = function()
+                              makeDeviceInfo()
+                           end
+
+   objectsButton = loveframes.Create("button", toolbar)
+   objectsButton:SetPos(toolbar:GetWidth() - 105, 5)
+   objectsButton:SetSize(100, 25)
+   objectsButton:SetText("See elements")
+   objectsButton.OnClick = function()
+                              makeObjects()
+                           end
+end
+
+function makeDeviceInfo()
+
+end
+
+
+function makeChooseDevice()
+   local frame = loveframes.Create("frame")
+   frame:SetName("Path to device")
+   frame:SetSize(500, 90)
+   frame:SetModal(true)
+   local textinput = loveframes.Create("textinput", frame)
+   textinput:SetPos(5, 30)
+   textinput:SetWidth(490)
+   textinput.OnEnter = function(object)
+			  loadArduino(textinput:GetText())
+			  frame:Remove()
+		       end
+   textinput:SetFont(love.graphics.newFont(12))
+   local togglebutton = loveframes.Create("button", frame)
+   togglebutton:SetPos(5, 60)
+   togglebutton:SetWidth(490)
+   togglebutton:SetText("OK")
+   togglebutton.OnClick = function(object)
+			     loadArduino(textinput:GetText())
+			     frame:Remove()
+			  end
+end
